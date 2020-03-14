b59588150f173dd2d6f94471c745e983ca6d71a6
Vishnu Unnikrishnan <optra7@gmail.com>
2020-03-14 18:23:05 +0530

    Visualization

--- Rec.R
+++ Rec.R
@@ -63,3 +63,64 @@ names(recommendation_model)
 
 lapply(recommendation_model, "[[", "description")
 
+recommendation_model$IBCF_realRatingMatrix$parameters
+
+similarity_mat <- similarity(ratingMatrix[1:4, ],
+                             method = "cosine",
+                             which = "users")
+as.matrix(similarity_mat)
+image(as.matrix(similarity_mat), main = "User's Similarities")
+
+movie_similarity <- similarity(ratingMatrix[, 1:4], method =
+                                 "cosine", which = "items")
+as.matrix(movie_similarity)
+image(as.matrix(movie_similarity), main = "Movies similarity")
+
+
+rating_values <- as.vector(ratingMatrix@data)
+unique(rating_values)
+
+
+Table_of_Ratings <- table(rating_values)
+Table_of_Ratings  
+
+#vis
+
+movie_views <- colCounts(ratingMatrix) # count views for each movie
+table_views <- data.frame(movie = names(movie_views),
+                          views = movie_views) # create dataframe of views
+table_views <- table_views[order(table_views$views,
+                                 decreasing = TRUE), ] # sort by number of views
+table_views$title <- NA
+for (index in 1:10325){
+  table_views[index,3] <- as.character(subset(movie_data,
+                                              movie_data$movieId == table_views[index,1])$title)
+}
+table_views[1:6,]
+
+ggplot(table_views[1:6, ], aes(x = title, y = views)) +
+  geom_bar(stat="identity", fill = 'steelblue') +
+  geom_text(aes(label=views), vjust=-0.3, size=3.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Total Views of the Top Films")
+
+#heat map
+
+image(ratingMatrix[1:20, 1:25], axes = FALSE, main = "Heatmap of the first 25 rows and 25 columns")
+
+#Data Preperation
+
+movie_ratings <- ratingMatrix[rowCounts(ratingMatrix) > 50,
+                              colCounts(ratingMatrix) > 50]
+movie_ratings
+
+
+minimum_movies<- quantile(rowCounts(movie_ratings), 0.98)
+minimum_users <- quantile(colCounts(movie_ratings), 0.98)
+image(movie_ratings[rowCounts(movie_ratings) > minimum_movies,
+                    colCounts(movie_ratings) > minimum_users],
+      main = "Heatmap of the top users and movies")
+
+average_ratings <- rowMeans(movie_ratings)
+qplot(average_ratings, fill=I("steelblue"), col=I("red")) +
+  ggtitle("Distribution of the average rating per user")
